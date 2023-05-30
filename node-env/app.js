939c350eeb6aeda4c026bfb37999c69d9fa90517
calli97 <nahuelcallalli@gmail.com>
2023-05-30 14:55:07 -0300

    setting env variables in dockerfile

--- node-env/app.js
+++ node-env/app.js
@@ -10,6 +10,10 @@ app.get("/", (req, res) => {
     res.json({ msg: "OK" });
 });
 
+app.get("/env", (req, res) => {
+    res.json({ msg: process.env });
+});
+
 app.listen(3001, () => {
     console.log("Server running on port 3001");
 });

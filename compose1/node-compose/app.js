0741bfc7406febbd38433fd62c83c0dc5a7a6f96
calli97 <nahuelcallalli@gmail.com>
2023-05-31 22:07:15 -0300

    compose pg and node

--- compose1/node-compose/app.js
+++ compose1/node-compose/app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
+const { Client } = require("pg");
+const client = new Client(process.env.DATABASE_URL);
+
 app.use(cors());
 
 app.use(express.json());
@@ -14,6 +17,12 @@ app.get("/env", (req, res) => {
     res.json({ msg: process.env });
 });
 
-app.listen(3001, () => {
+app.listen(3001, async () => {
     console.log("Server running on port 3001");
+    try {
+        await client.connect();
+        console.log("Database Successfully connect");
+    } catch (error) {
+        console.log(error);
+    }
 });
